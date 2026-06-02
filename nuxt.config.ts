79f8b4039aeb9e1ce7eeb8ee270fd81a369747e2
mityatube <mityatube@gmail.com>
2026-06-02 16:08:51 +0300

    deploy setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: false,
     app: {
+        baseURL: '/art-salon-3/',
         head: {
             title: "Художественный салон",
             meta: [
