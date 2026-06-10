ba743cfac29a282961f97a5b02d49155b806ecd2
mityatube <mityatube@gmail.com>
2026-06-10 16:34:58 +0300

    PT Serif

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,19 @@ export default defineNuxtConfig({
                     rel: 'icon',
                     type: 'image/svg+xml',
                     href: '/logoVyatkaArt.svg'
+                },
+                {
+                    rel: 'preconnect',
+                    href: 'https://fonts.googleapis.com'
+                },
+                {
+                    rel: 'preconnect',
+                    href: 'https://fonts.gstatic.com',
+                    crossorigin: ''
+                },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap'
                 }
             ]
         },
